88d2447e5267f4a87e51132b022ad94ae5281c86
Gage Krumbach <gkrumbach@gmail.com>
2021-06-22 09:04:12 -0500

    changed route constants to work with github pages

--- src/navigation/CONSTANTS.js
+++ src/navigation/CONSTANTS.js
@@ -1,3 +1,6 @@
-export const ROOT = "/";
-export const DASHBOARD = "/dashboard";
-export const SEARCH = "/search";
+// sub route is used to work with git hub pages as the domain contains multiple apps
+const SUBROUTE = "thoth-search/";
+
+export const ROOT = "/" + SUBROUTE;
+export const DASHBOARD = ROOT + "dashboard";
+export const SEARCH = ROOT + "search";
