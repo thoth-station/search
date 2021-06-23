f188fd231d27bbdbbb77498268ce2d49c77d21c6
Gage Krumbach <gkrumbach@gmail.com>
2021-06-22 19:18:16 -0500

    building tree works but is slow

--- src/components/Dashboard/PackageDependencies.js
+++ src/components/Dashboard/PackageDependencies.js
@@ -3,18 +3,26 @@ import React, { useState, useEffect } from "react";
 
 // local components
 import NetworkGraph from "components/Metrics/DependenciesMetric/NetworkGraph.js";
+import ErrorPage from "components/Shared/ErrorPage";
 
 // utils
 import { createDependencyGraph } from "utils/createDependencyGraph";
-import { getScaleFreeNetwork } from "utils/getScaleFreeNetwork";
 
 // material-ui
 import { Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import LoadingErrorTemplate from "components/Shared/LoadingErrorTemplate";
+
+//vis-dataset
+import { DataSet } from "vis-network/standalone/esm/vis-network";
 
 const useStyles = makeStyles(theme => ({
   graph: {
     padding: theme.spacing(4)
+  },
+  progress: {
+    marginLeft: "50%",
+    marginTop: "25%"
   }
 }));
 
@@ -23,21 +31,39 @@ const PackageDependencies = ({ data }) => {
   const [graphData, setGraphData] = useState(undefined);
 
   useEffect(() => {
-    createDependencyGraph(data.info.name, data.info.version).then(r => {
-      setGraphData(r);
-    });
+    createDependencyGraph(data.info.name, data.info.version)
+      .then(r => {
+        console.log(r);
+        setGraphData({
+          nodes: new DataSet(r.nodes),
+          edges: new DataSet(r.edges)
+        });
+      })
+      .catch(error => {
+        setGraphData("error");
+        console.log(error);
+      });
   }, [data]);
 
   return (
-    <Paper>
-      {graphData !== undefined ? (
+    <LoadingErrorTemplate
+      state={
+        graphData === undefined
+          ? "loading"
+          : graphData === "error"
+          ? "error"
+          : undefined
+      }
+      errorPage={<ErrorPage text={"Could not load in dependency graph"} />}
+    >
+      <Paper>
         <NetworkGraph
           data={graphData}
           className={classes.graph}
           root={data.info.name + data.info.version}
         />
-      ) : null}
-    </Paper>
+      </Paper>
+    </LoadingErrorTemplate>
   );
 };
 
--- src/utils/createDependencyGraph.js
+++ src/utils/createDependencyGraph.js
@@ -6,34 +6,42 @@ import compareVersions from "tiny-version-compare";
 // with a graph of all package dependencies.
 // It uses Thoth API to get dependencies.
 // And uses PyPi to get metadata
-export function createDependencyGraph(name, version) {
-  const nodes = [];
-  const edges = [];
+export async function createDependencyGraph(
+  name,
+  version,
+  nodes = [],
+  edges = [],
+  haveVisited = [],
+  depth = 0
+) {
+  // add initial node
+  if (depth === 0) {
+    nodes.push({
+      id: name + version,
+      label: name
+      //data: res.data
+    });
+  }
 
-  nodes.push({
-    id: name + version,
-    label: name
-    //data: res.data
-  });
+  depth += 1;
 
   // get initial dep list
-  return thothGetDependencies(name, version)
-    .then(r => {
-      // sort the dependencies by date using pypi metadata
-      let initDeps = r.data.dependencies;
-      initDeps = initDeps.sort(function(a, b) {
-        return compareVersions(a.version, b.version);
-      });
-      return initDeps;
-    })
-    .then(async initDeps => {
-      // recurse into each dep
-      for (let i = initDeps.length - 1; i >= 0; i--) {
-        // only include one version of a dep (latest)
-        // get metadata of packages
+  return await thothGetDependencies(name, version).then(async r => {
+    // sort the dependencies by date using pypi metadata
+    let initDeps = r.data.dependencies;
+    initDeps = initDeps.sort(function(a, b) {
+      return compareVersions(a.version, b.version);
+    });
 
+    for (let i = initDeps.length - 1; i >= 0; i--) {
+      // only include one version of a dep (latest)
+      if (!haveVisited.includes(initDeps[i].name.toLowerCase())) {
+        // add dep to lookup table
+        haveVisited.push(initDeps[i].name.toLowerCase());
+
+        // get metadata of packages
         await searchForPackage(initDeps[i].name, initDeps[i].version).then(
-          res => {
+          async res => {
             // create a network node of dep
             nodes.push({
               id: initDeps[i].name + initDeps[i].version,
@@ -46,13 +54,43 @@ export function createDependencyGraph(name, version) {
               to: name + version,
               from: initDeps[i].name + initDeps[i].version
             });
+
+            // recurse into dependency's dependencies
+            // if there are dependencies
+            if (
+              res.data.info.requires_dist !== null &&
+              res.data.info.requires_dist.length !== 0 &&
+              depth < 2
+            ) {
+              return await createDependencyGraph(
+                initDeps[i].name,
+                initDeps[i].version,
+                nodes,
+                edges,
+                haveVisited,
+                depth
+              ).then(r => {
+                return {
+                  nodes: r.nodes,
+                  edges: r.edges
+                };
+              });
+            } else {
+              return {
+                nodes: nodes,
+                edges: edges
+              };
+            }
           }
         );
+        // update the nodes and edges
+        // nodes.concat(ps.nodes);
+        // edges.concat(ps.edges);
       }
-
-      return {
-        nodes: new DataSet(nodes),
-        edges: new DataSet(edges)
-      };
-    });
+    }
+    return {
+      nodes: nodes,
+      edges: edges
+    };
+  });
 }
