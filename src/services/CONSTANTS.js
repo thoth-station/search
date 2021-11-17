34be959fc1b12468be17ad2a1244f79008a2de95
Gage Krumbach <gkrumbach@gmail.com>
2021-11-17 14:23:50 +0000

    quick change

--- src/services/CONSTANTS.js
+++ src/services/CONSTANTS.js
@@ -1,4 +1,3 @@
 export const PYPI = "https://pypi.org/pypi";
 export const THOTH = "https://khemenu.thoth-station.ninja/api/v1/";
-export const LICENSES =
-  "https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json";
+export const LICENSES = "https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json";
